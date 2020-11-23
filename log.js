418ebf31c1b72d5b7c3dfaf462208d2b51e255c0
U-C-S <ucs3721@outlook.com>
2020-11-23 11:07:32 +0530

    Update log.js

--- log.js
+++ log.js
@@ -6,11 +6,12 @@ module.exports = (x = "Here") => {
 
 /*
 ####ToDO
-- Function to copy the files to dist (x)
 - feature to excute other commands before the execution of this package
 - feature to deploy the dist folder to Gh-pages
 - srcgh.json
 
+//Completed
+- Function to copy the files to dist
 - maybe add all the create a union of all similar items, like all files list in a single array and same for dirs
 - Use better naming
 */
