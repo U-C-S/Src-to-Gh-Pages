9ee3cb132d64a9e4b9acbcec94c433f2e85c022f
U-C-S <ucs3721@outlook.com>
2020-11-23 11:04:30 +0530

    0.4.0 - Fixed issue from 0.4.0-beta1

--- copy.js
+++ copy.js
@@ -2,7 +2,13 @@ const fs = require("fs");
 const log = require("./log");
 
 function copier(data) {
-  fs.mkdirSync("dist");
+  if (fs.existsSync("dist")) {
+    fs.rmdirSync("dist", { recursive: true });
+    fs.mkdirSync("dist");
+  } else {
+    fs.mkdirSync("dist");
+  }
+
   data.dirArray.forEach((dir) => {
     let newDirPath = dir.replace("src", "dist");
     fs.mkdirSync(newDirPath);
