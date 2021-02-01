791434ee752961490fd2884bbc1583894459e549
U-C-S <ucs3721@outlook.com>
2021-02-01 17:49:25 +0530

    Using ES6 classes

--- dir.js
+++ dir.js
@@ -1,7 +1,75 @@
 const fs = require("fs");
 const path = require("path");
 
+class theDirReader {
+  constructor(entryDir, dirBindAddress) {
+    this.entryDir = entryDir;
+    this.dirBindAddress = dirBindAddress;
+    this.Addresses = {
+      rootDir: "",
+      dirArray: [],
+      fileArray: [],
+      dirFullPath: [],
+      fileFullPath: [],
+    };
+  }
+
+  Out() {
+    let entt = (this.Addresses.rootDir = path.join(this.dirBindAddress, this.entryDir));
+    this.dirReader(entt);
+    for (let i = 0; i < this.Addresses.dirFullPath.length; i++) {
+      this.dirReader(this.Addresses.dirFullPath[i]);
+    }
+
+    return this.Addresses;
+  }
+
+  dirReader(src) {
+    let readDir = fs.readdirSync(src);
+    let { rootDir } = this.Addresses;
+
+    readDir.forEach((address) => {
+      let fullPath = path.join(src, address);
+      let relPath = path.relative(rootDir, fullPath);
+      let pathStats = fs.statSync(fullPath);
+
+      if (pathStats.isDirectory()) {
+        this.Addresses.dirArray.push(relPath);
+        this.Addresses.dirFullPath.push(fullPath);
+      } else {
+        this.Addresses.fileArray.push(relPath);
+        this.Addresses.fileFullPath.push(fullPath);
+      }
+    });
+  }
+
+  static Copier(SrcDirData, CopyTarget) {
+    if (fs.existsSync(CopyTarget)) {
+      fs.rmdirSync(CopyTarget, { recursive: true });
+      fs.mkdirSync(CopyTarget);
+    } else {
+      fs.mkdirSync(CopyTarget);
+    }
+
+    SrcDirData.dirArray.forEach((dir) => {
+      let destFullPath = path.join(CopyTarget, dir);
+      fs.mkdirSync(destFullPath);
+    });
+
+    SrcDirData.fileArray.forEach((file) => {
+      let destFullPath = path.join(CopyTarget, file);
+      let srcFilePath = path.join(SrcDirData.rootDir, file);
+      fs.copyFileSync(srcFilePath, destFullPath);
+    });
+
+    console.log(`Copied Files to ${CopyTarget} Directory`);
+  }
+}
+
+module.exports = theDirReader;
+
 //-----DirReader----------------
+/*
 let Addresses = {
   rootDir: "",
   dirArray: [],
@@ -61,4 +129,7 @@ function Copier(SrcDirData, CopyTarget) {
   console.log(`Copied Files to ${CopyTarget} Directory`);
 }
 
+
+
 module.exports = { ThePathsOf, Copier };
+*/
